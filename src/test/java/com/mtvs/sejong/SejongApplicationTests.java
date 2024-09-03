de17b87605adb8a7594dfaa511a6f5604e7063dd
INUK-ai <arheon2469@gmail.com>
2024-09-03 13:47:33 +0900

    Setting: Build.gradle

---
+++ src/test/java/com/mtvs/sejong/SejongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtvs.sejong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SejongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
