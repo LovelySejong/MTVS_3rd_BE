a7dd1b07f3e7939096b11b66280a12cdd94c7dc3
DVLP-Seok <dookie289@gmail.com>
2024-09-05 09:10:22 +0900

    Feat: QuestionController - createQuestion

--- src/main/java/com/mtvs/sejong/question/application/controller/QuestionController.java
+++ src/main/java/com/mtvs/sejong/question/application/controller/QuestionController.java
@@ -1,5 +1,8 @@
 package com.mtvs.sejong.question.application.controller;
 
+import com.mtvs.sejong._core.utils.ApiUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,5 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/question")
 public class QuestionController {
 
-    @PostMapping
+    @GetMapping
+    public ResponseEntity<?> createQuestion() {
+
+        return ResponseEntity.ok().body(ApiUtils.success(null));
+    }
 }
