165c05a110655176e34a0b260ff5312e0606adc9
INUK-ai <arheon2469@gmail.com>
2024-09-11 18:28:38 +0900

    Feat: localServerStart - newUser

--- src/main/java/com/mtvs/sejong/SejongApplication.java
+++ src/main/java/com/mtvs/sejong/SejongApplication.java
@@ -1,8 +1,16 @@
 package com.mtvs.sejong;
 
+import com.mtvs.sejong.user.domain.Authority;
+import com.mtvs.sejong.user.domain.User;
+import com.mtvs.sejong.user.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableFeignClients
@@ -12,4 +20,22 @@ public class SejongApplication {
         SpringApplication.run(SejongApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner localServerStart(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            userRepository.saveAll(Arrays.asList(
+                    newUser("USER1", "user1@test.com", "test1234", passwordEncoder),
+                    newUser("USER2", "user2@test.com", "test1234", passwordEncoder)
+            ));
+        };
+    }
+
+    private User newUser(String nickName, String email, String password, PasswordEncoder passwordEncoder) {
+        return User.builder()
+                .nickname(nickName)
+                .email(email)
+                .password(passwordEncoder.encode(password))
+                .authority(Authority.USER)
+                .build();
+    }
 }
