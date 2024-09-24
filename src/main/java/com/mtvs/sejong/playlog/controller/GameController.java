a2e7a14a2353053aeab9b35e1fb0cabd8330383a
INUK-ai <arheon2469@gmail.com>
2024-09-24 16:22:02 +0900

    Feat: GameController - enterGame

---
+++ src/main/java/com/mtvs/sejong/playlog/controller/GameController.java
@@ -0,0 +1,27 @@
+package com.mtvs.sejong.playlog.controller;
+
+import com.mtvs.sejong._core.utils.ApiUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/game")
+public class GameController {
+
+    private final GameService gameService;
+
+    @PostMapping
+    public ResponseEntity<?> enterGame(@RequestBody GameRequestDTO.createGameDTO requestDTO) {
+
+        gameService.createGame(requestDTO);
+
+        return ResponseEntity.ok().body(ApiUtils.success(null));
+    }
+}
