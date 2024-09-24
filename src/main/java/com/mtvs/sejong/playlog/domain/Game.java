28c7e35148e0a131e5683ef896700308b66ec7d5
INUK-ai <arheon2469@gmail.com>
2024-09-24 16:16:46 +0900

    Feat: Game Entity

---
+++ src/main/java/com/mtvs/sejong/playlog/domain/Game.java
@@ -0,0 +1,30 @@
+package com.mtvs.sejong.playlog.domain;
+
+import com.mtvs.sejong.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "tbl_user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Game extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private Long userId;
+    @Column
+    private Long partnerId;
+
+    @Builder
+    public Game(Long userId, Long partnerId) {
+        this.userId = userId;
+        this.partnerId = partnerId;
+    }
+}
